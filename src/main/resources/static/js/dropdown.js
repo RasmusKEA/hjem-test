a7e1d2894f34a4ea8885afe99ee7bc58ec9de12a
unknown <rasm937k@stud.kea.dk>
2021-05-18 16:28:35 +0200

    create user

---
+++ src/main/resources/static/js/dropdown.js
@@ -0,0 +1,13 @@
+function account(){
+    const targetDiv = document.getElementById("formthing")
+    targetDiv.style.display = "block";
+}
+
+function animation(){
+    const targetDiv = document.getElementById("formthing")
+    targetDiv.style.display = "none";
+}
+
+function featured(){
+    console.log("featured item")
+}
