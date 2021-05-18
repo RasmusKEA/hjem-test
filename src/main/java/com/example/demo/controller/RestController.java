ebd837ccd2e58f83c0d1959980010aa1f58156dd
unknown <rasm510f@stud.kea.dk>
2021-05-18 17:37:15 +0200

    employee

--- src/main/java/com/example/demo/controller/RestController.java
+++ src/main/java/com/example/demo/controller/RestController.java
@@ -1,12 +1,41 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.Authority;
+import com.example.demo.model.Employee;
+import com.example.demo.repository.AuthorityRepository;
 import com.example.demo.repository.EmployeeRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
     private final EmployeeRepository employeeRepository;
+    private final AuthorityRepository authorityRepository;
+    private PasswordEncoder passwordEncoder;
 
-    public RestController(EmployeeRepository employeeRepository){
+
+    public RestController(EmployeeRepository employeeRepository, AuthorityRepository authorityRepository){
+        this.authorityRepository = authorityRepository;
         this.employeeRepository = employeeRepository;
     }
+
+
+
+
+
+    @PostMapping(value = "/createEmployee", consumes = "application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Employee createEmployee(@RequestBody Employee employee){
+        Authority admin = authorityRepository.save(Authority.builder().role("ROLE_ADMIN").build());
+        Authority user = authorityRepository.save(Authority.builder().role("ROLE_USER").build());
+        Authority customer = authorityRepository.save(Authority.builder().role("ROLE_CUSTOMER").build());
+
+        return employeeRepository.save(Employee.builder().username(employee.getUsername()).password(passwordEncoder.encode(employee.getPassword())).authority(user).build());
+
+
+    }
 }
+
--- src/main/resources/static/js/dropdown.js
+++ src/main/resources/static/js/dropdown.js
@@ -11,3 +11,43 @@ function animation(){
 function featured(){
     console.log("featured item")
 }
+
+
+
+function createAccount() {
+    let name = document.getElementById("name");
+    let username = document.getElementById("email");
+    let password = document.getElementById("password");
+    let password2 = document.getElementById("password2");
+
+    let newEmployee = {
+
+        "username" : `${username.value}`,
+        "password" : `${password.value}`
+    };
+
+    let body = JSON.stringify(newEmployee);
+
+    const URL = "http://localhost:8080/createEmployee";
+
+    const postObject = {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        method: "POST",
+        body: body
+    };
+
+    if (password.value === password2.value){
+        fetch(URL, postObject)
+            .then(response => response.json())
+            .then(data =>{
+                console.log("Succes!", data)
+            }).catch((error) => {
+                console.log("error", error)
+        });
+
+    }
+
+}
+
